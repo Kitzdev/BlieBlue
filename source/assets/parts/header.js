48308654ddab815ff40677cc6013e0e28b26ba16
Kitbert <kitbert77@gmail.com>
2022-03-28 22:40:03 +0700

    Menambahkan header.js dan menambahkan script untuk merender header pada semua halaman.

---
+++ source/assets/parts/header.js
@@ -0,0 +1,21 @@
+document.write(`
+    <div class="d-flex align-items-center justify-content-around bg-transparent flex-wrap header-container">
+    <div class="d-flex navbar-brand align-items-center">
+        <img src="assets/images/header/header-logo.png" alt="header-logo">
+        <span class="d-inline"><span id="header_blieblue_text">BlieBlue</span> PetShop</span>
+    </div>
+    <div class="d-flex navbar-brand justify-content-around flex-wrap me-5 navbar-anchor">
+        <a href="/source/landing.page.html" class="fs-6 px-4">HOME</a>
+        <a href="/source/about.us.html" class="fs-6  px-4">ABOUT US</a>
+        <a href="/source/products.html" class="fs-6  px-4">PRODUCTS</a>
+        <a href="/source/services.html" class="fs-6 px-4">SERVICES</a>
+        <a href="/source/contact.us.html" class="fs-6 px-4">CONTACT US</a>
+    </div>
+    <div class="d-flex navbar-brand justify-content-around flex-wrap">
+        <img src="assets/images/header/bx_bxl-facebook.png" alt="Facebook Logo" class="px-3">
+        <img src="assets/images/header/bx_bxl-instagram.png" alt="Instagram Logo" class="px-3">
+        <img src="assets/images/header/bi_twitter.png" alt="Twitter Logo" class="px-3">
+        <img src="assets/images/header/ci_mail.png" alt="Email Logo" class="px-3">
+    </div>
+</div>
+`);
