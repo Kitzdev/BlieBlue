c45627b488e16e9d8991f640cffdcf79771c5574
VannCristian <96682438+VannCristian@users.noreply.github.com>
2022-04-03 19:48:15 +0700

    add file

---
+++ source/assets/parts/footer.js
@@ -0,0 +1,30 @@
+document.write(`
+    <div class="footer-container container-fluid">
+        <div class="footer-navigation">
+            <h2>BlieBlue Petshop</h2>
+            <div class="navigation">
+                <a href="products.html">Product</a>
+                <a href="services.html">Service</a>
+                <a href="about.us.html">About</a>
+                <a href="contact.us.html">Contact</a>
+            </div>
+        </div>
+        <div class="footer-detail">
+            <p>Stay Connected!</p>
+            <div class="footer-social-media">
+                <img alt="Facebook Logo" src="assets/images/footer/bx_bxl-facebook.svg">
+                    <img alt="Instagram Logo" src="assets/images/footer/bx_bxl-instagram.svg">
+                        <img alt="Twitter Logo" src="assets/images/footer/bi_twitter.svg">
+                            <img alt="Email Logo" src="assets/images/footer/ci_mail.svg">
+            </div>
+            <p>(021) 690 9090</p>
+            <p>Jl. Ancol Barat IV, RT.12/RW.2, Ancol, Kec. Pademangan, Kota Jkt Utara, Daerah Khusus Ibukota Jakarta
+                14430</p>
+        </div>
+    </div>
+<div class="footer-copyright container-fluid">
+    <p>
+        Copyright &copy; 2022 - Blieblue | All Right Reserved!
+    </p>
+</div>
+`);
