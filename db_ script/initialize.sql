c0005deed68337a3049f2563fbca07bb9fc41c5f
Kitbert <kitbert77@gmail.com>
2022-06-06 22:39:35 +0700

    Fix sql syntax

--- db_ script/initialize.sql
+++ db_ script/initialize.sql
@@ -1,30 +1,37 @@
 begin;
 
-create table item (
-    item_id integer primary key auto_increment,
-    item_name text,
-    price DECIMAL(10, 2),
-    item_type integer,
+create table item
+(
+    item_id     integer AUTO_INCREMENT primary key,
+    item_name   text,
+    price       DECIMAL(10, 2),
+    item_type   integer,
     description text,
-    flag integer,
-)
+    flag        integer
+);
 
-create table user (
-    user_id integer auto_increment primary key,
+create table user
+(
+    user_id       integer AUTO_INCREMENT primary key,
     user_fullname text,
-    user_name text,
-    password text, -- hash menggunakan md5 atau sha256
-    user_role integer
-)
+    user_name     text,
+    password      text, -- hash menggunakan md5 atau sha256
+    user_role     integer
+);
 
-create table cart (
-    cart_id integer primary key auto_increment,
-    user_id integer foreign key references user(user_id),
-)
+create table cart
+(
+    cart_id integer AUTO_INCREMENT primary key,
+    user_id integer,
+    foreign key (user_id) references user(user_id)
+);
 
-create table cart_item (
-    item_id integer foreign key references item(item_id),
-    cart_id integer foreign key references cart(cart_id),
-)
+create table cart_item
+(
+    item_id integer,
+    cart_id integer,
+    foreign key (item_id) references item(item_id),
+    foreign key (cart_id) references cart(cart_id)
+);
 
 commit;
