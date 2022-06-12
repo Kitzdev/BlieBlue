08c02aae48b41611bf16c061856939ff5e0a2b19
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-06-12 22:06:33 +0700

    Config database connection at config/database.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function showProduct() {
+        $data_member = DB::table('tb_member')->get();
+        return $data_member;
+    }
 }
