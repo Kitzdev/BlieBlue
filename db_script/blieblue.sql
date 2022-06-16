e53648736474196e7ccd0f77f00bc74671b6237d
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-06-16 17:24:38 +0700

    Insert images into project, image_name into database

---
+++ db_script/blieblue.sql
@@ -0,0 +1,44 @@
+begin;
+
+-- Create tables
+create table item
+(
+    item_id     integer AUTO_INCREMENT primary key,
+    item_name   text,
+    price       DECIMAL(10, 2),
+    item_type   integer,
+    description text,
+    flag        integer,
+    image_path  TEXT
+);
+
+create table user
+(
+    user_id       integer AUTO_INCREMENT primary key,
+    user_fullname text,
+    user_name     text,
+    password      text, -- hash menggunakan md5 atau sha256
+    user_role     varchar(30)
+);
+
+create table cart
+(
+    cart_id integer AUTO_INCREMENT primary key,
+    user_id integer,
+    foreign key (user_id) references user(user_id)
+);
+
+create table cart_item
+(
+    item_id integer,
+    cart_id integer,
+    foreign key (item_id) references item(item_id),
+    foreign key (cart_id) references cart(cart_id)
+);
+
+-- Insert data
+
+
+
+
+commit;
