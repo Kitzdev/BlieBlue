d50f2fe0f4b4975dbf5454c287c8151ce3f4055a
VannCristian <96682438+VannCristian@users.noreply.github.com>
2022-06-16 15:05:46 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' =>  env('APP_URL') .'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -47,7 +47,7 @@ return [
         'public_uploads' => [
             'driver' => 'local',
             'root'   => public_path() . '/storage/uploads',
-            'url' => url('uploads'),
+            'url' =>  env('APP_URL') . '/public/storage/uploads',
         ],
 
         's3' => [
